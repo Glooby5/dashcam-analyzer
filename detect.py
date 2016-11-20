8d73444748cf9c7b3a92068083c109ab0beb8844
Jan Kaderabek <kaderabek.jan@gmail.com>
2016-11-20 16:47:16 +0100

    #2 Training
    - zrychlení detekce

--- detect.py
+++ detect.py
@@ -1,37 +1,40 @@
 import cv2
 import argparse
 
+MAX_WIDTH = 150
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", help="path to the cascade", required=True)
 ap.add_argument("-v", "--video", help="path to the video", required=True)
 args = vars(ap.parse_args())
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-# this is the cascade we just made. Call what you want
-watch_cascade = cv2.CascadeClassifier(args['file'])
+cascade = cv2.CascadeClassifier(args['file'])
 
 cap = cv2.VideoCapture(args['video'])
 frameCount = 0
 
 while 1:
-    if frameCount % 5:
+    ret, frame = cap.read()
+
+    if frameCount % 2:
+        frameCount += 1
         continue
 
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # add this
-    # image, reject levels level weights.
-    watches = watch_cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=2, minSize=(20, 20))
+    watches = cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=2, minSize=(20, 20))
 
     # add this
     for (x, y, w, h) in watches:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        if w > MAX_WIDTH or h > MAX_WIDTH:
+            continue
 
-    cv2.imshow('img', img)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+
+    cv2.imshow('img', frame)
     k = cv2.waitKey(30) & 0xff
+    frameCount += 1
+
     if k == 27:
         break
 
